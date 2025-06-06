bccc9a0eea37e7d19d94e8819d5ee9c9443d196a
Pogozhikhh <Pogozhikhh@yandex.ru>
2025-06-06 17:29:24 +0300

    additional task completed

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home_html(request):
     return render(request, 'home.html')
 
 def contacts_html(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+
     return render(request, 'contacts.html')
