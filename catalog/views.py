7a0c2bd7719a47cec1e8b973656d95b358718d79
Pogozhikhh <Pogozhikhh@yandex.ru>
2025-06-23 19:45:40 +0300

    complete homework 25

--- catalog/views.py
+++ catalog/views.py
@@ -2,32 +2,30 @@ from gc import get_objects
 
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView
 
 from catalog.models import Product
 
 
-def home_html(request):
-    return render(request, "home.html")
+class HomeListView(ListView):
+    model = Product
+    template_name = "home.html"
+    context_object_name = "products"
 
 
-def contacts_html(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        message = request.POST.get("message")
-        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-
-    return render(request, "contacts.html")
+class ContactsListView(ListView):
+    model = Product
+    template_name = "contacts.html"
 
 
-def products_html(request):
-    products = Product.objects.all()
-    context = {
-        'products': products
-    }
-    return render(request, 'products.html', context)
+class ProductListView(ListView):
+    model = Product
+    template_name = "products.html"
+    context_object_name = "products"
 
-def single_product_html(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product" : product}
-    return render(request, "single_product.html", context)
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "product_detail.html"
+    context_object_name = "product"
